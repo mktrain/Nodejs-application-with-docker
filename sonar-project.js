cf44ac1f6fb35f686bc392455b8e1ae01e830d2a
mktrain <80453983+mktrain@users.noreply.github.com>
2021-11-15 14:12:46 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://34.93.111.157:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJs',
+	    'sonar.login': 'eaed64d74c779f0727c752e034dfca22c2f7af62',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
